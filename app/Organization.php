2fa92f880ec1d0590c3dc4b51295a2f7ee06f070
Rob Grieshaber <rob@lgd-it.us>
2017-09-01 13:59:13 -0500

    Created Organization model, and seeder classes for Member, Organization and Role

--- app/Organization.php
+++ app/Organization.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Organization extends Model
 {
-    //
+    protected $table = 'organizations';
+	
+	protected $fillable = [
+            'name', 'address1', 'address2', 'city', 'state', 'zipcode', 'database_file', 'phone_main', 'phone_alt'];
+	
+	protected $hidden = [];
+
+	public function users() {
+		return $this->hasMany('App\User');
+	}
+	
+	public function project13s() {
+		return $this->hasMany('App\Project13');
+	}
 }
