c46e79f93904770f5fc8b0d8572c20ef48d307bd
Rob Grieshaber <rob@lgd-it.us>
2018-01-14 23:34:18 -0600

    Added Super Datagrid, copying its demo public/assets in its entirety. Just now beginning to explore it, haven't changed any P13 documents yet. Deleted all EasyUI files and folders

--- public/crud-demo/conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$conn = @mysql_connect('127.0.0.1','response_p13acc2','P!34ccDB');
-if (!$conn) {
-	die('Could not connect: ' . mysql_error());
-}
-mysql_select_db('response_p13acc2', $conn);
-
-?>
